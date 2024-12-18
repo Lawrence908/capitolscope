de1e900cfba3638263833534f1da9f1b53f0aab8
Chris Lawrence <c.lawrence908@gmail.com>
2024-12-17 22:43:23 -0800

    Looped over all functioning years, will need to refine parsing "list out of index" by trial and error going through 'bad_docs' to treat edge cases. Then re fetch all years data

--- src/ingestion/fetch_stock_data.py
+++ src/ingestion/fetch_stock_data.py
@@ -54,6 +54,7 @@
 
 import os
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 from dotenv import load_dotenv
 
@@ -62,8 +63,16 @@ load_dotenv()
 ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 BASE_URL = os.getenv("ALPHA_VANTAGE_BASE_URL")
 
-
-
+def get_tickers():
+    url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    table = soup.find('table', {'id': 'constituents'})
+    tickers500 = []
+    for row in table.find_all('tr')[1:]:
+        ticker = row.find('td').text.strip()
+        tickers500.append(ticker)
+    return tickers500
 
 def fetch_time_series(symbol: str, interval: str = "daily", output_size: str = "compact") -> pd.DataFrame:
     """
